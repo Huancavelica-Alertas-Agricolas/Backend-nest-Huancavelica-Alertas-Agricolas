b69c4988bb7194056b305e565fa19ba7b2c3ff2b
JuanAldairRamirezMendez <u20201597@utp.edu.pe>
2025-10-12 11:07:34 -0500

    eliminando: scripts en scripts/; usar .githooks

--- scripts/import_logs.ps1
+++
@@ -1,7 +0,0 @@
-# Importar datos ETL en logs_db
-# Uso: powershell -File scripts\import_logs.ps1
-
-$repoRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
-docker cp "$repoRoot\sql\etl\logs_data.sql" agro_postgres_logs:/tmp/logs_data.sql
-docker exec -e PGPASSWORD=admin agro_postgres_logs psql -U admin -d logs_db -f /tmp/logs_data.sql
-docker exec -e PGPASSWORD=admin agro_postgres_logs psql -U admin -d logs_db -c "SELECT COUNT(*) FROM logs;"
